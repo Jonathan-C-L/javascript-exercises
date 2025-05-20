7978c3d96e4d11267c34612ed569e8421e179247
Jonathan <jle16@nait.ca>
2025-05-20 17:18:30 -0600

    13_palindromes completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let swapedWord = "";
+    for(let i = word.length -1; i >= 0; i--){
+        swapedWord += word[i];
+    }
+    if (swapedWord === word)
+        return true;
+    return false;
 };
 
 // Do not edit below this line

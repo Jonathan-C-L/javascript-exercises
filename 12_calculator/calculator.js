b389dc2eb5e092c9e3cfc95f4c565f93cd1fa19a
Jonathan <jle16@nait.ca>
2025-05-20 17:01:08 -0600

    12_calculator assignment completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i<array.length; i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  return num1*num2;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let total = 0;
+  for (let p = 0; p < power; p++){
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = num;
+  for (let n = num; n > 0; n--){
+    total *= (num-n);
+  }
+  return total;
 };
 
 // Do not edit below this line

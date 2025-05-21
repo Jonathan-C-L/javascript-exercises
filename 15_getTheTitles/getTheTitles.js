e90dac762d6feabae3ddfb7510adfc4fe4890ee2
Jonathan <jle16@nait.ca>
2025-05-21 01:22:47 -0600

    find the oldest assignment completed

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
-const getTheTitles = function(books) {
-    let titles = [];
-    for(let i = 0; i < books.length; i++){
-        titles.push(books[i].title);
-    }
-    return titles;
+const getTheTitles = function(array) {
+  return array.map((book) => book.title);
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(calculateOldest);
 };
+
+function calculateOldest(currentOldest, person){
+    let date = new Date();
+
+    let nextPerson = (person.yearOfDeath != undefined)?(person.yearOfDeath-person.yearOfBirth):(date.getFullYear()-person.yearOfBirth);
+    let prevPerson = (currentOldest.yearOfDeath != undefined)?(currentOldest.yearOfDeath-currentOldest.yearOfBirth):(date.getFullYear()-currentOldest.yearOfBirth);
+    
+    if(nextPerson > prevPerson)
+        return person;
+    else
+        return currentOldest; 
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
